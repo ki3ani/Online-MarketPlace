9aa630a76391c3c16c549cbd9f5cc043c3d955fb
ki3ani <kimanim68@gmail.com>
2023-11-28 19:30:17 +0300

    adding-jupyter-notebooks

--- app.py
+++ app.py
@@ -1,35 +1,22 @@
 from flask import Flask, jsonify, request
-import pandas as pd
-from scipy.stats import zscore
-import numpy as np
-import scipy.stats as stats
-from scipy.stats import kruskal
-import scikit_posthocs as sp
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.metrics.pairwise import cosine_similarity
-from recommendation import load_datasets, get_similar_items, recommend_items
-from recommendation import file_paths
+import recommendation
 
-
-# Your existing code here...
+# Load datasets
+data = recommendation.load_datasets(recommendation.file_paths)
 
 app = Flask(__name__)
 
-@app.route('/load_datasets', methods=['GET'])
-def load_datasets_route():
-    data = load_datasets(file_paths)
-    return jsonify(success=True)
+@app.route('/recommend_items/<user_id>', methods=['GET'])
+def recommend_items_route(user_id):
+    user_id = float(user_id)
+    recommended_items = recommendation.recommend_items(user_id, data)
+    return jsonify(recommended_items=recommended_items)
 
 @app.route('/get_similar_items/<product_id>', methods=['GET'])
 def get_similar_items_route(product_id):
-    similar_items = get_similar_items(int(product_id))
-    return jsonify(similar_items=similar_items.tolist())
-
-@app.route('/recommend_items/<user_id>', methods=['GET'])
-def recommend_items_route(user_id):
-    recommended_items = recommend_items(float(user_id))
-    return jsonify(recommended_items=recommended_items.tolist())
+    product_id = float(product_id)
+    similar_items = recommendation.get_similar_items(product_id, data)
+    return jsonify(similar_items=similar_items)
 
 if __name__ == '__main__':
     app.run(debug=True)
